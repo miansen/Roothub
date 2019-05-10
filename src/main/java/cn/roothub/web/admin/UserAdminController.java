a8caf67d8b50bd85665ec6b90c6a83990620bb99
MianSenWang <1158827539@qq.com>
2019-05-10 23:00:32 +0800

    修复前台传 String 类型的时间数据，映射 Controller 的 Date 类型失败的问题

--- src/main/java/cn/roothub/web/admin/UserAdminController.java
+++ src/main/java/cn/roothub/web/admin/UserAdminController.java
@@ -1,15 +1,21 @@
 package cn.roothub.web.admin;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.context.request.WebRequest;
 import cn.roothub.dto.Result;
 import cn.roothub.entity.User;
 import cn.roothub.service.UserService;
@@ -58,5 +64,18 @@ public class UserAdminController {
 	@ResponseBody
 	public Result<String> refreshToken(){
 		return new Result<>(true, StringUtil.getUUID());
-	} 
+	}
+	
+	/**
+	 * 局部日期转换，将 String 类型的时间数据转化为 Date 类型
+	 * @param binder
+	 * @param request
+	 */
+	@InitBinder
+    public void initBinder(WebDataBinder binder, WebRequest request) {
+        // 转换日期 注意这里的转化要和传进来的字符串的格式一直 如2015-9-9 就应该为yyyy-MM-dd
+        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
+        // CustomDateEditor为自定义日期编辑器
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
 }

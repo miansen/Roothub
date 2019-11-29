8226e0a1ea38e12729140b076d337b1910546c5f
MianSenWang <1158827539@qq.com>
2019-11-29 23:38:26 +0800

    delete MyLoader

--- src/main/java/cn/roothub/bbs/core/io/support/PathMatchingResourcePatternResolver.java
+++ src/main/java/cn/roothub/bbs/core/io/support/PathMatchingResourcePatternResolver.java
@@ -76,23 +76,5 @@ public class PathMatchingResourcePatternResolver implements ResourcePatternResol
 		}
 		return result.toArray(new Resource[result.size()]);
 	}
-
-	public static class MyLoader extends ClassLoader {
-		@Override
-		public Class<?> loadClass(String name) throws ClassNotFoundException {
-			try {
-				String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
-				InputStream is = getClass().getResourceAsStream(fileName);
-				if (is == null) {
-                    return super.loadClass(name);
-                }
-				byte[] b = new byte[is.available()];
-				is.read(b);
-				return defineClass(name, b, 0, b.length);
-			} catch (IOException e) {
-				throw new ClassNotFoundException(name);
-			}
-		}
-	}
 	
 }

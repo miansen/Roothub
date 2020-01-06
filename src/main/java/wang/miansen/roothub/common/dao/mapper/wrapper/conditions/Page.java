293b94f86c399a86f59c828837cecada247ccbb8
miansen <1158827539@qq.com>
2020-01-06 09:45:36 +0800

    新增 Page 接口，支持分页功能。

---
+++ src/main/java/wang/miansen/roothub/common/dao/mapper/wrapper/conditions/Page.java
@@ -0,0 +1,26 @@
+package wang.miansen.roothub.common.dao.mapper.wrapper.conditions;
+
+/**
+ * 分页接口
+ * 
+ * @param <R> 返回结果的类型
+ * 
+ * @author miansen.wang
+ * @date 2020-01-05
+ * @since 3.0
+ */
+public interface Page<R> {
+
+	/**
+	 * limit 语句
+	 * 
+	 * <p>例：limit("0", "10") -> limit 0,10
+	 * <p>注意：仅支持 MySQL 数据库
+	 * 
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	R limit(String start, String end);
+	
+}

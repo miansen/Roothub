628345c707f4c41e00f0f997835ccb37a68553e9
miansen <1158827539@qq.com>
2020-01-09 10:27:25 +0800

    更改 TagServiceImpl

--- src/main/java/wang/miansen/roothub/modules/tag/service/impl/TagServiceImpl.java
+++ src/main/java/wang/miansen/roothub/modules/tag/service/impl/TagServiceImpl.java
@@ -1,6 +1,9 @@
 package wang.miansen.roothub.modules.tag.service.impl;
 
+import java.util.function.Function;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import wang.miansen.roothub.common.dao.BaseDao;
 import wang.miansen.roothub.common.service.impl.AbstractBaseServiceImpl;
@@ -14,48 +17,45 @@ import wang.miansen.roothub.modules.tag.service.TagService;
  * @author miansen.wang
  * @date 2020-01-04
  */
+@Service
 public class TagServiceImpl extends AbstractBaseServiceImpl<Tag, TagDTO> implements TagService {
 
 	@Autowired
 	private ITagDao tagDao;
-	
+
 	@Override
-	public Tag getBaseDO(TagDTO dto) {
-		Tag tag = new Tag();
-		tag.setTagId(dto.getTagId());
-		tag.setTagName(dto.getTagName());
-		tag.setTagAvatar(dto.getTagAvatar());
-		tag.setTagContent(dto.getTagContent());
-		tag.setTagState(dto.getTagState());
-		tag.setCreateDate(DateUtil.string2Date(dto.getCreateDate(), DateUtil.FORMAT_DATETIME));
-		tag.setUpdateDate(DateUtil.string2Date(dto.getUpdateDate(), DateUtil.FORMAT_DATETIME));
-		return tag;
+	public Function<? super TagDTO, ? extends Tag> getDTO2DOMapper() {
+		return tagDTO -> {
+			Tag tag = new Tag();
+			tag.setTagId(tagDTO.getTagId());
+			tag.setTagName(tagDTO.getTagName());
+			tag.setTagAvatar(tagDTO.getTagAvatar());
+			tag.setTagContent(tagDTO.getTagContent());
+			tag.setTagState(tagDTO.getTagState());
+			tag.setCreateDate(DateUtil.string2Date(tagDTO.getCreateDate(), DateUtil.FORMAT_DATETIME));
+			tag.setUpdateDate(DateUtil.string2Date(tagDTO.getUpdateDate(), DateUtil.FORMAT_DATETIME));
+			return tag;
+		};
 	}
 
-	/* (non-Javadoc)
-	 * @see wang.miansen.roothub.common.service.BaseService#getBaseDao()
-	 */
 	@Override
-	public BaseDao<Tag> getBaseDao() {
+	public Function<? super Tag, ? extends TagDTO> getDO2DTOMapper() {
+		return tagDO -> {
+			TagDTO tagDTO = new TagDTO();
+			tagDTO.setTagId(tagDO.getTagId());
+			tagDTO.setTagName(tagDO.getTagName());
+			tagDTO.setTagAvatar(tagDO.getTagAvatar());
+			tagDTO.setTagContent(tagDO.getTagContent());
+			tagDTO.setTagState(tagDO.getTagState());
+			tagDTO.setCreateDate(DateUtil.formatDateTime(tagDO.getCreateDate()));
+			tagDTO.setUpdateDate(DateUtil.formatDateTime(tagDO.getUpdateDate()));
+			return tagDTO;
+		};
+	}
+
+	@Override
+	public BaseDao<Tag> getDao() {
 		return tagDao;
 	}
-
-	/* (non-Javadoc)
-	 * @see wang.miansen.roothub.common.service.BaseService#getBaseDTO(wang.miansen.roothub.common.entity.BaseDO)
-	 */
-	@Override
-	public TagDTO getBaseDTO(Tag entity) {
-		TagDTO tagDTO = new TagDTO();
-		tagDTO.setTagId(entity.getTagId());
-		tagDTO.setTagName(entity.getTagName());
-		tagDTO.setTagAvatar(entity.getTagAvatar());
-		tagDTO.setTagContent(entity.getTagContent());
-		tagDTO.setTagState(entity.getTagState());
-		tagDTO.setCreateDate(DateUtil.formatDateTime(entity.getCreateDate()));
-		tagDTO.setUpdateDate(DateUtil.formatDateTime(entity.getUpdateDate()));
-		return tagDTO;
-	}
-
-	
 
 }
